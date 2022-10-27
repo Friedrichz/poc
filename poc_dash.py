77821a377281088f7b82929bb9f6fc3cf62d3ce5
Friedrichz <afz2105@columbia.edu>
2022-10-27 18:36:23 +0200

    file dump

---
+++ poc_dash.py
@@ -0,0 +1,279 @@
+from sys import stdout
+import streamlit as st
+from PIL import Image
+
+import poc_model_script as ms
+#import assets, ret_df, li_ss, fig_rets_dic, fillcolors_regimes, regime_li, add_regimes, port_rets
+
+bslogo = Image.open('./assets/logo-brightside.png')
+
+# Page configs
+st.set_page_config(
+    page_title="BS Portfolio Model",
+    page_icon="🧊",
+    # layout="wide",
+)
+
+
+# Header
+header_col1, header_col2 = st.columns([1, 3], gap='medium')
+
+with header_col1:
+    st.image(bslogo, width=100)
+
+with header_col2:
+    st.title('Portfolio Allocation Model Proof of Concept')
+
+# Conceptual walkthrough
+conceptimg = Image.open('./assets/conceptual_flowchart_v2-grey.png')
+
+st.empty()
+
+# _____________________________________________________________________________
+#
+# Description
+# _____________________________________________________________________________
+
+st.empty()
+st.markdown(
+    """
+    ## Intro
+
+    This presents a proof of concept for how to systematize parts of our portfolio allocation methodology going forward
+
+    The intent is to lay out a concise framework on how to quantitatively process both empirical data and discretionary views into a portfolio model that ultimately provides us with optimal portfolio weights. 
+
+    This model is kept intentionally simple. Each of these steps contains a set of non-trivial assumptions that that would need to be revisited in detail were we to follow through on this approach (see section at end).
+    """
+)
+
+st.image(conceptimg)
+
+st.markdown(
+    """
+    ## Guide
+
+    #### 1. Model Inputs ####
+    - **Empirical Distribution** of risk assets in our universe, for example Indices, Funds, Equities, etc. **(priors)**
+    
+    &rarr; For this example, I have selected a few 'proxy' assets to broadly mimick how we think about portfolio construction, namely:
+        *Offense* &rarr; SPX Index,
+        *Defense* &rarr; EHFI451 Index (Long Vol Index) & EHFI453 Index (Tail Risk Index),
+        *Cash* &rarr; DXY Curncy & XAU Curncy
+
+
+    - **Discretionary Views** on risk assets (relative or absolute), for example GIP model, upper/lower return bounds for asset, asset class X will outperform asset class Y, etc.
+    
+    &rarr; For this example, I'm using a simplified version of **Hedgeyes GIP model** to classify historical timeseries into one of the 4 quadrants. These regimes are determined by two factors only, namely the rate of change in QoQ GDP and QoQ CPI.
+    I'm then using these regimes to estimate regime-conditional return distributions for each asset, essentially making expected returns for each asset dependent on the (expected) regime.
+
+    - A priori **portfolio constraints**, for example Equities exposure > 10 % at all times, Gold never < 40 %
+    
+    &rarr; In this case, I'm ommitting any additional discretionary views.
+
+    #### 2. Construct posterior asset distributions ####
+    This is where we combine prior estimates of returns (e.g. market implied returns) with views on certain assets, to produce a **posterior** estimate of expected returns.
+    The mathematical model behind this is called **Black-Litterman**.
+
+
+    #### 3. Solve for optimal portfolio weights according to pre-defined objective function ####
+    The traditional metric to optimize for is **mean-variance**, which I'm also using here for ilustrative purposes
+    Other objective functions may include max return, min risk, max risk adj return ratio, max utility, mean downside variance, etc. 
+
+    This results in **portfolio weights** optimized for the selected objective.
+    
+    &rarr; Here we're solving for a max sharpe ratio
+
+    #### 4. Backtesting ####
+    Finally, the weights get tested both against historical out of sample or simulated forward looking data (Monte Carlo).
+    
+    &rarr; Here I'm doing a quick in-sample backtest without any post-processing or stress testing of weights 
+
+    ________________________________________________________________________________________________________________________________
+    """
+)
+
+
+# _____________________________________________________________________________
+#
+# Estimating returns assets
+# _____________________________________________________________________________
+
+# Create figure w/ regimes
+fig_cret = ms.port_crets.drop(columns=['Portfolio']).plot()
+# fig_cret = ms.add_regimes(fig_cret, ms.regime_li)
+fig_cret.update_layout(height=500, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", 
+                        legend=dict(orientation="h"), 
+                        margin=dict(l=0,r=0,b=0,t=0)
+                        )
+
+
+# Returns regime cond
+fig_ret = ms.ret_df.drop(columns=['regime']).plot.bar()
+fig_ret.update_layout(height=500, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", 
+                        legend=dict(orientation="h"),
+                        margin=dict(l=0,r=0,b=0,t=0)
+                        )
+
+
+# GIP regime model 
+fig_gip_model = ms.feat_df[['EHPIUS Index','GDP CQOQ Index']].plot()
+fig_gip_model = ms.add_regimes(fig_gip_model, ms.regime_li)
+fig_gip_model.update_layout(height=500, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", 
+                        legend=dict(orientation="h"),
+                        margin=dict(l=0,r=0,b=0,t=0)
+                        )
+
+
+# Display charts 
+st.empty()
+
+
+st.header('Model Inputs')
+st.subheader('Proxy Assets')
+st.plotly_chart(fig_cret, use_container_width=True)
+
+model_inputs_cols = st.columns(2) 
+with model_inputs_cols[0]:
+    st.subheader('Raw returns')
+    st.plotly_chart(fig_ret)
+
+with model_inputs_cols[0]:
+    st.subheader('GIP Model: Inflation & GDP')
+    st.plotly_chart(fig_gip_model)
+
+# Describe return tables (whole data)
+hist_returns_cols1, hist_returns_cols2 = st.columns([3, 1], gap='medium')
+    
+with hist_returns_cols1:
+    st.subheader('Returns Summary')
+    st.write(ms.ret_df.describe())
+
+with hist_returns_cols2:
+    st.subheader('Sample Size')
+    st.write(ms.ret_df.regime.value_counts())
+
+
+# Historical return assets conditional on regime analysis
+st.empty()
+st.header('Expected Returns')
+st.subheader('Return distribution across regimes')
+
+asset_rets_tabs = st.tabs(ms.assets)
+for i, name in enumerate(ms.assets):
+    tab_content = asset_rets_tabs[i]
+    with tab_content:
+            st.plotly_chart(ms.fig_rets_dic[name])
+
+
+# Regime conditional moments
+st.subheader('Distribution Moments conditioned on regimes')
+reg_names = [i['regime'] for i in ms.li_ss]
+regimes_ss_tabs = st.tabs(reg_names)
+
+for i,r in enumerate(ms.li_ss):
+    with regimes_ss_tabs[i]:
+        st.write(r['regime'])
+        dist_mom1, dist_mom2 = st.columns([1, 3], gap='medium')
+    
+        with dist_mom1:
+            st.caption('Mean')
+            st.write(r['mean'])
+
+        with dist_mom2:
+            st.caption('Covariance')
+            st.write(r['cov'])
+
+
+# _____________________________________________________________________________
+# 
+# Optimization
+# _____________________________________________________________________________
+st.empty()
+st.header('Optimization Model Results')
+
+st.caption('Objective:') 
+st.latex(r'''
+    max \quad \mu / \sigma
+    ''')
+
+st.caption('Constraints: Apart from non-negativity of weights no constraints were imposed') 
+
+st.subheader('Efficient Frontier across Regimes')
+regimes_eff_tabs = st.tabs(reg_names)
+
+for i,r in enumerate(ms.li_eff):
+    with regimes_eff_tabs[i]:
+        er, vol, sr = r['ef'].portfolio_performance(verbose=True)
+        st.write(r['regime'])
+
+        st.caption('Optimizer Solution')
+        st.text(
+            """
+            Expected annual return: {}% 
+            Annual volatility: {}%
+            Sharpe Ratio: {}%
+            """.format(round(er,2)*100, round(vol,2)*100, round(sr,2)*100)
+        )
+
+        st.empty()
+        st.caption('Optimal weights')
+        st.write(r['weights'])
+
+# _____________________________________________________________________________
+# 
+# Backtest
+# _____________________________________________________________________________
+st.empty()
+st.header('Backtest')
+
+# Create backtest figure w/ regimes
+fig_cret = ms.port_crets.plot()
+
+fig_cret = ms.add_regimes(fig_cret, ms.regime_li)
+fig_cret.update_layout(height=500, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", 
+                        legend=dict(orientation="h"),
+                        margin=dict(l=0,r=0,b=0,t=0)
+                        )
+
+st.plotly_chart(fig_cret, use_container_width=True)
+
+st.subheader('Weights')
+fig_wbtest = ms.ts_weights.plot()
+fig_wbtest = ms.add_regimes(fig_wbtest, ms.regime_li)
+fig_wbtest.update_layout(height=300, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", 
+                        legend=dict(orientation="h"),
+                        margin=dict(l=0,r=0,b=0,t=0)
+                        )
+
+st.plotly_chart(fig_wbtest, use_container_width=True)
+
+
+# Tearsheet
+# qs.plots.snapshot(port_rets.Portfolio, title='Portfolio Performance')
+
+st.empty()
+st.markdown(
+    """
+    ________________________________________________________________________________________________________________________________
+
+    ## Key Considerations
+
+    #### Ad 1. Model Inputs ####
+    - Two major challenges with empirical data
+    * 1. **Proxy returns**: Since we're allocating to funds with limited track records and non-trivial risk-return profiles we will either need to find proxies or replicate their return characteristics (think of long vol)
+    * 2. **Sample size**: As you can see I've included a table with number of observations above for each regime. As an example, 13 data points for Quad4 regime make any statistic/inference about dynamics within that regime superflous given statistical insignificance. 
+    In a similar vein, a hedge fund with a 3yr track record of monthly returns provides a very limited basis to understand how it will react in different regimes. Thus, we might have to resort to simulating risk profiles.
+
+    - Translating discretionary views into mathematical syntax is also not straightforward but key to making forward-looking allocations. 
+    (Last 10yrs of market data will tell us nothing about next 10yrs) This is were our true edge comes in.
+
+
+    #### Ad 3. Optimization ####
+    - Mathematical optimization is a very fragile undertaking so understanding the objective function and parameter sensibility is paramount. 
+    Clearly, we will not want to stick to a linear model like MVO, particularly in the light of the highly non-liner payoff profiles we're looking to be exposed to.
+    This will mean iterating through a large amount of objective functions, stress testing results against a series of scenarios, etc.
+
+
+    """
+)
---
+++ poc_model_script.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+from pypfopt.efficient_frontier import EfficientFrontier
+
+pd.options.plotting.backend = "plotly"
+
+assets = ['SPX Index','DXY Curncy','EHFI451 Index','EHFI453 Index','XAU Curncy']
+proxy_assets = dict(
+    offense = ['SPX Index'],
+    defence = ['EHFI451 Index','EHFI453 Index','XAU Curncy'],
+    cash = ['DXY Curncy'],
+)
+
+# Load data & format
+data_df = pd.read_csv('.\data\poc_data.csv')
+data_df.columns = ['Date'] + data_df.columns.tolist()[1:]
+data_df['Date'] = pd.to_datetime(data_df.Date)
+# print(data_df.shape)
+data_df = data_df.dropna(how='any')
+data_df = data_df.set_index('Date')
+# print(data_df.shape)
+
+# Classify regimes
+# Resample to quarterly data just to classify regimes
+qdata_df = data_df[['EHPIUS Index','GDP CQOQ Index']].resample('Q', convention='start').asfreq()
+# print(qdata_df.shape)
+
+# Assign regimes
+feat_df = qdata_df.copy()
+feat_df['EHPIUS Index'] = feat_df['EHPIUS Index'].diff(1)
+feat_df = feat_df.dropna()
+feat_df['regime'] = np.nan
+
+feat_df['regime'] = np.where((feat_df['EHPIUS Index']<0) & (feat_df['GDP CQOQ Index']>0),"Quad 1", 0)
+feat_df['regime'] = np.where((feat_df['EHPIUS Index']>0) & (feat_df['GDP CQOQ Index']>0),"Quad 2", feat_df['regime'])
+feat_df['regime'] = np.where((feat_df['EHPIUS Index']>0) & (feat_df['GDP CQOQ Index']<0),"Quad 3", feat_df['regime'])
+feat_df['regime'] = np.where((feat_df['EHPIUS Index']<0) & (feat_df['GDP CQOQ Index']<0),"Quad 4", feat_df['regime'])
+
+# Label monthly data (regime)
+data_df1 = data_df[assets]
+
+data_df2 = data_df1.merge(feat_df[['regime']], 'left', left_index=True, right_index=True)
+data_df2['regime'] = data_df2['regime'].ffill()
+# print(data_df2.shape)
+
+data_df3 = data_df2.dropna(subset=['regime'])
+# print(data_df3.shape)
+
+# Returns df (monthly)
+ret_df = data_df3.copy()
+ret_df[assets] = ret_df[assets].pct_change()
+ret_df = ret_df.dropna()
+
+
+# Estimate distributions conditional on regime
+def sumstats(df, regime):
+    mu = mean_historical_return(df, returns_data=True, frequency=12, compounding=False)
+    S = CovarianceShrinkage(df,  returns_data=True, frequency=12).ledoit_wolf()
+    return dict(regime=regime, mean=mu, cov=S)
+
+
+li_ss = [sumstats(group.drop(columns=['regime']), name) for name, group in ret_df.groupby('regime')]
+# len(li_ss)
+
+# Plot asset returns conditional on regime
+fig_rets_dic = {}
+
+for a in assets: 
+    fig = px.histogram(ret_df, x=a, color="regime",
+                   marginal="box", # or violin, rug
+                   nbins=30,
+                   )
+    fig.update_layout(height=500, paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    fig_rets_dic[a] = fig
+
+
+# Optimization conditional on regime
+def eff_front(dic):
+    ef = EfficientFrontier(dic['mean'], dic['cov'])
+    weights = ef.max_sharpe()
+    cleaned_weights = ef.clean_weights()
+    return dict(regime=dic['regime'], ef=ef, weights=cleaned_weights)
+
+li_eff = [eff_front(i) for i in li_ss]
+
+
+# Create regime weighting map
+rw_df = pd.DataFrame([dict(regime=i['regime'], **i['weights']) for i in li_eff])
+rw_df = rw_df.set_index('regime')
+
+
+# Backtest cond allocations
+ts_regimes = ret_df[['regime']]
+ts_weights = rw_df.merge(ret_df[['regime']], how='right', left_index=True, right_on='regime').drop(columns=['regime'])
+
+# Porfolio weighted returns
+port_rets = ret_df.copy().drop(columns=['regime'])
+# print(ts_weights.shape == port_rets.shape)
+port_rets['Portfolio'] = (port_rets * ts_weights).sum(axis=1)
+
+# Cum rets
+port_crets = port_rets.add(1).cumprod()
+
+
+# Plotting regimes
+ts_regimes1 = ts_regimes.copy()
+ts_regimes1['regime_start'] = ts_regimes[ts_regimes.regime != ts_regimes.regime.shift(1)]
+ts_regimes1['regime_end'] = ts_regimes[ts_regimes.regime != ts_regimes.regime.shift(-1)]
+
+regime_li = []
+
+for i in list(zip(ts_regimes[ts_regimes.regime != ts_regimes.regime.shift(1)].reset_index().to_records(), \
+    ts_regimes[ts_regimes.regime != ts_regimes.regime.shift(-1)].reset_index().to_records())):
+    regime_li += [(i[0][2], pd.Timestamp(i[0][1]).replace(day=1).strftime('%Y-%m-%d'), i[1][1])]
+
+
+fillcolors_regimes = {
+    'Quad 1': "green",
+    'Quad 2': "blue",
+    'Quad 3': "yellow",
+    'Quad 4': "LightSalmon",
+}
+
+def add_regimes(fig, regimes, fillcolors_regimes=fillcolors_regimes):
+    for i in regimes:
+        start = i[1]
+        end = i[2].astype(str)[:10]
+
+        # Add shape regions
+        fig.add_vrect(
+            x0=start, x1=end,
+            fillcolor=fillcolors_regimes[i[0]], opacity=0.2,
+            layer="below", line_width=0,
+        )
+    return fig
+
+
+# # Colorcode regimes
+# def highlight_rows(val, fillcolors_regimes=fillcolors_regimes):
+#     return 'background-color: {}'.format(fillcolors_regimes[val])
+
+
+# reg_df = ret_df.regime.value_counts().reset_index()
